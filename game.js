2aceeb837314e44c6abe9e02a866aca742a0b4fb
Ze'ev Lailari <lailari.zeev@gmail.com>
2018-03-01 15:57:20 -0800

    Make background fit canvas size

--- game.js
+++ game.js
@@ -106,8 +106,6 @@ function loadImages() {
     dogImage = dogRPara;
     para = new Image();
     para.src = (("game/pics/Para.png"));
-    bg = new Image();
-    bg.src = (("game/pics/SpaceBackground.jpg"));
 }
  
 var keysDown = {};
@@ -279,10 +277,8 @@ function popsEnforceBounds() {
 }
  
 function render() {
-    // ctx.fillStyle = "#FFFFFF";
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
     if (startRender){
-        ctx.drawImage(bg,0,0);
+        drawBackground();
 
         for(var i=0; i<numAnemies;i++) {
             ctx.fillStyle = '#00F';
@@ -299,11 +295,60 @@ function render() {
 function resetPops() {
     onBlock = -1;
     pops.x_vel = 0;
-    
 }
 
-function bounce(i) {
+/**
+ * Set background image to canvas size
+ */
+function drawBackground() {
+    drawImageProp(ctx, bg);
+}
+
+function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
     
+    // default offset is center
+    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+    
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+    
+    var iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih),
+        nw = iw * r, // new prop. width
+        nh = ih * r, // new prop. height
+        cx, cy, cw, ch, ar = 1;
+    
+    // decide which gap to fill
+    if (nw < w) ar = w / nw;
+    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh; // updated
+    nw *= ar;
+    nh *= ar;
+    
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+    
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+    
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+    
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch, x, y, w, h);
 }
 
 function collisions() {
